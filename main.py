b808dda00c7b1920701a56b7560f961e498a70bb
Minyen Hsieh <minyen.hsieh.1997@gmail.com>
2023-07-16 12:31:23 -0500

    Successfully connecting to SharePoint and retrieving a specific folder's files.

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from settings import ICF_USERNAME, ICF_PASSWORD
 
-print(ICF_USERNAME, ICF_PASSWORD)
+from office365.runtime.auth.user_credential import UserCredential
+# The ClientContext class is used to interact with SharePoint data and services.
+from office365.sharepoint.client_context import ClientContext
+
+
+site_url = 'https://icfonline.sharepoint.com/sites/DMTechnologyStandards'
+credentials = UserCredential(ICF_USERNAME, ICF_PASSWORD)
+ctx = ClientContext(site_url).with_credentials(credentials)
+
+# retrieve specific folder.
+list_source = ctx.web.get_folder_by_server_relative_url(
+    'Shared Documents/1. Program & Project Support/B. Client Specific/LA ORM/Data/Damages')
+files = list_source.files
+ctx.load(files)
+
+# This line sends the request to SharePoint to execute the operations accumulated in the context (ctx) so far.
+# This is known as "batching" and is commonly used to improve performance when working with SharePoint.
+ctx.execute_query()
+
+for file in files:
+    print(file.properties['Name'])
