92e7aa33c0d699a997632f9a8731e9a71ac43b32
Minyen Hsieh <minyen.hsieh.1997@gmail.com>
2023-07-16 18:37:39 -0500

    successfully downloaded the data from sharepoint to local environment.

--- main.py
+++ main.py
@@ -3,25 +3,48 @@ from settings import ICF_USERNAME, ICF_PASSWORD
 from office365.runtime.auth.user_credential import UserCredential
 # The ClientContext class is used to interact with SharePoint data and services.
 from office365.sharepoint.client_context import ClientContext
+from pathlib import Path
 
+import os
 import fnmatch
 
 site_url = 'https://icfonline.sharepoint.com/sites/DMTechnologyStandards'
 credentials = UserCredential(ICF_USERNAME, ICF_PASSWORD)
 ctx = ClientContext(site_url).with_credentials(credentials)
 
+folder_url = 'Shared Documents/1. Program & Project Support/B. Client Specific/LA ORM/Data/Damages'
+print('folder_url:', folder_url)
+
 # retrieve specific folder.
-list_source = ctx.web.get_folder_by_server_relative_url(
-    'Shared Documents/1. Program & Project Support/B. Client Specific/LA ORM/Data/Damages')
+list_source = ctx.web.get_folder_by_server_relative_url(folder_url)
 files = list_source.files
 ctx.load(files)
-
-# This line sends the request to SharePoint to execute the operations accumulated in the context (ctx) so far.
-# This is known as "batching" and is commonly used to improve performance when working with SharePoint.
+#
+# # This line sends the request to SharePoint to execute the operations accumulated in the context (ctx) so far.
+# # This is known as "batching" and is commonly used to improve performance when working with SharePoint.
 ctx.execute_query()
-
 file_names = [file.properties['Name'] for file in files]
+print('file_names:', file_names)
 
-target_files = "*damage*"
-matching_files = fnmatch.filter(file_names, target_files)
-print(matching_files[0])
+target_wildcard = "*damage*"
+matching_files_names = fnmatch.filter(file_names, target_wildcard)
+target_file_name = matching_files_names[-1]
+print('target_file_name:', target_file_name)
+
+
+target_file_url = folder_url + "/" + target_file_name
+print('target_file_url:', target_file_url)
+
+# print(Path.home())
+local_data_folder = os.path.join(Path.home(), r'Documents\ICF\Automation\Damages')
+print('local_data_folder:', local_data_folder)
+
+download_path = os.path.join(local_data_folder, os.path.basename(target_file_url)).replace('\\', '/')
+print('download_path:', download_path)
+# C:/Users/60788/Documents/ICF/Automation/Damages/Damages Export.csv
+
+# This line opens a file on the local file system for writing in binary mode.
+with open(download_path, "wb") as local_file:
+    file = ctx.web.get_file_by_server_relative_url(target_file_url).download(local_file).execute_query()
+
+print("[Ok] file has been downloaded into: {0}".format(download_path))
