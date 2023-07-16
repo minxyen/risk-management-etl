a6d56347143f0ad5db7810c4b56670491f6d2e7d
Minyen Hsieh <minyen.hsieh.1997@gmail.com>
2023-07-16 15:10:35 -0500

    retrieving file names by using wild card

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from office365.runtime.auth.user_credential import UserCredential
 # The ClientContext class is used to interact with SharePoint data and services.
 from office365.sharepoint.client_context import ClientContext
 
+import fnmatch
 
 site_url = 'https://icfonline.sharepoint.com/sites/DMTechnologyStandards'
 credentials = UserCredential(ICF_USERNAME, ICF_PASSWORD)
@@ -19,5 +20,8 @@ ctx.load(files)
 # This is known as "batching" and is commonly used to improve performance when working with SharePoint.
 ctx.execute_query()
 
-for file in files:
-    print(file.properties['Name'])
+file_names = [file.properties['Name'] for file in files]
+
+target_files = "*damage*"
+matching_files = fnmatch.filter(file_names, target_files)
+print(matching_files[0])
