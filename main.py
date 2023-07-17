27e07d274d377469b7ccf4db7d4457ad8a9f1081
Minyen Hsieh <minyen.hsieh.1997@gmail.com>
2023-07-17 12:26:07 -0500

    Converted SITE_URL, WILDCARD_FILENAME, LOCAL_DATA_FOLDER into environment variables

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from settings import ICF_USERNAME, ICF_PASSWORD
+from settings import ICF_USERNAME, ICF_PASSWORD, SITE_URL, WILDCARD_FILENAME, LOCAL_DATA_FOLDER
 
 from office365.runtime.auth.user_credential import UserCredential
 # The ClientContext class is used to interact with SharePoint data and services.
@@ -8,9 +8,9 @@ from pathlib import Path
 import os
 import fnmatch
 
-site_url = 'https://icfonline.sharepoint.com/sites/DMTechnologyStandards'
+# site_url = 'https://icfonline.sharepoint.com/sites/DMTechnologyStandards'
 credentials = UserCredential(ICF_USERNAME, ICF_PASSWORD)
-ctx = ClientContext(site_url).with_credentials(credentials)
+ctx = ClientContext(SITE_URL).with_credentials(credentials)
 
 folder_url = 'Shared Documents/1. Program & Project Support/B. Client Specific/LA ORM/Data/Damages'
 print('folder_url:', folder_url)
@@ -26,8 +26,8 @@ ctx.execute_query()
 file_names = [file.properties['Name'] for file in files]
 print('file_names:', file_names)
 
-target_wildcard = "*damage*"
-matching_files_names = fnmatch.filter(file_names, target_wildcard)
+# wildcard_filename = "*damage*"
+matching_files_names = fnmatch.filter(file_names, WILDCARD_FILENAME)
 target_file_name = matching_files_names[-1]
 print('target_file_name:', target_file_name)
 
@@ -36,10 +36,10 @@ target_file_url = folder_url + "/" + target_file_name
 print('target_file_url:', target_file_url)
 
 # print(Path.home())
-local_data_folder = os.path.join(Path.home(), r'Documents\ICF\Automation\Damages')
-print('local_data_folder:', local_data_folder)
+# local_data_folder = os.path.join(Path.home(), r'Documents\ICF\Automation\Damages')
+print('local_data_folder:', LOCAL_DATA_FOLDER)
 
-download_path = os.path.join(local_data_folder, os.path.basename(target_file_url)).replace('\\', '/')
+download_path = os.path.join(LOCAL_DATA_FOLDER, os.path.basename(target_file_url)).replace('\\', '/')
 print('download_path:', download_path)
 # C:/Users/60788/Documents/ICF/Automation/Damages/Damages Export.csv
 
--- settings.py
+++ settings.py
@@ -4,3 +4,9 @@ load_dotenv()
 
 ICF_USERNAME = os.getenv('ICF_USERNAME')
 ICF_PASSWORD = os.getenv('ICF_PASSWORD')
+
+
+SITE_URL = os.getenv('SITE_URL')
+WILDCARD_FILENAME = os.getenv('WILDCARD_FILENAME')
+
+LOCAL_DATA_FOLDER = os.getenv('LOCAL_DATA_FOLDER')
